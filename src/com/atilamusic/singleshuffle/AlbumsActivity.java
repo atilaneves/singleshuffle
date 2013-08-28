f0ea95774a5b02c8871b5fdb43cd137c9784ba79
Atila Neves <aalvesne@cisco.com>
2013-08-28 15:22:14 +0200

    Changed AlbumsActivity to derive from ListActivity

--- src/com/atilamusic/singleshuffle/AlbumsActivity.java
+++ src/com/atilamusic/singleshuffle/AlbumsActivity.java
@@ -1,17 +1,14 @@
 package com.atilamusic.singleshuffle;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class AlbumsActivity extends Activity /*implements LoaderManager.LoaderCallbacks<Cursor>*/ {
+public class AlbumsActivity extends ListActivity /*implements LoaderManager.LoaderCallbacks<Cursor>*/ {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_albums);
-        ListView albums = (ListView)findViewById(R.id.albums_list);
-        albums.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 new String[]{"Foo", "Bar", "Baz's greatest hits"}));
     }
 }
